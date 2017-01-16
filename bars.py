453f844a4886f16a3c8983af226844fb6add1903
arektar <zander5@mail.ru>
2017-01-16 14:35:51 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -1,51 +1,36 @@
 import json
 from math import hypot
-def load_data(filepath):
-    with open(filepath) as json_file:
-        bars_data=json_file.read()
+
+
+def load_data(file_path):
+    with open(file_path) as json_file:
+        bars_data = json_file.read()
     return json.loads(bars_data)
 
 
 def get_biggest_bar(data):
-    biggest=["",0]
-    for bar in data:
-        if bar['SeatsCount']>biggest[1]:
-            biggest[1]=bar['SeatsCount']
-            biggest[0]=bar['Name']
-    return biggest
+    biggest = max(data, key=lambda x: x['SeatsCount'])
+    return biggest['Name'], biggest['SeatsCount']
 
 
 def get_smallest_bar(data):
-    biggest = ["", 1000000]
-    for bar in data:
-        if bar['SeatsCount'] < biggest[1]:
-            biggest[1] = bar['SeatsCount']
-            biggest[0] = bar['Name']
-    return biggest
+    smallest = min(data, key=lambda x: x['SeatsCount'])
+    return smallest['Name'], smallest['SeatsCount']
 
 
 def get_closest_bar(data, longitude, latitude):
-    nearest = ["",1000000, None,None]
-    for bar in data:
-        bar_longitude=bar['geoData']['coordinates'][0]
-        bar_latitude=bar['geoData']['coordinates'][1]
-        distance=hypot(abs(longitude - bar_longitude) , abs(latitude - bar_latitude))
-        if distance < nearest[1]:
-            nearest[0] = bar['Name']
-            nearest[1] = distance
-            nearest[2] = bar_longitude
-            nearest[3] = bar_latitude
-    return nearest
+    nearest = min(data, key=lambda x: hypot(
+        abs(longitude - x['geoData']['coordinates'][0]), abs(latitude - x['geoData']['coordinates'][1])))
+    return nearest['Name'], nearest['geoData']['coordinates'][0], nearest['geoData']['coordinates'][1]
 
 if __name__ == '__main__':
-    filepath=input("Введите полное имя файла со списком баров (.json): ")
-    bars_data=load_data(filepath)
-    biggest_bar=get_biggest_bar(bars_data)
-    print("Самый большой бар: " + biggest_bar[0] + " " + str(biggest_bar[1]) + " мест.")
-    smallest_bar=get_smallest_bar(bars_data)
-    print("Самый маленький бар: " + smallest_bar[0] + " " + str(smallest_bar[1]) + " мест.")
-    my_longitude=float(input("Введите свою долготу: "))
-    my_latitude=float(input("Введите свою широту: "))
-    closest_bar=get_closest_bar(bars_data,my_longitude,my_latitude)
-    print("Ближайший бар: " + closest_bar[0] + " координаты: " + str(closest_bar[3]) + ", " + str(closest_bar[2]))
-    
+    my_file_path = input("Введите полное имя файла со списком баров (.json): ")
+    my_bars_data = load_data(my_file_path)
+    biggest_bar = get_biggest_bar(my_bars_data)
+    print("Самый большой бар: %s %d   мест." % (biggest_bar[0], biggest_bar[1]))
+    smallest_bar = get_smallest_bar(my_bars_data)
+    print("Самый маленький бар: %s %d мест." % (smallest_bar[0], smallest_bar[1]))
+    my_longitude = float(input("Введите свою долготу: "))
+    my_latitude = float(input("Введите свою широту: "))
+    closest_bar = get_closest_bar(my_bars_data, my_longitude, my_latitude)
+    print("Ближайший бар: %s координаты: %f, %f" % (closest_bar[0], closest_bar[2], closest_bar[1]))
